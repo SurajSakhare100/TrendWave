9e288109df6159c6b109e2bdc2f6b1e1279ce860
suraj sakhare <sakharesuraj10@gmail.com>
2024-12-26 21:08:31 +0530

    logo updated

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -138,8 +138,7 @@ function ShoppingHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/shop/home" className="flex items-center gap-2">
-          <HousePlug className="h-6 w-6" />
-          <img src={Logo} className="h-6 w-6"/>
+          <img src={Logo} className="h-10 w-10"/>
           <span className="font-bold text-lg">Trend Wave</span>
         </Link>
         <Sheet>
